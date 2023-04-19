e0aa47dc2319564557c9766c70f2413cd805c013
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-04-19 10:53:20 -0400

    first 3

---
+++ index.js
@@ -0,0 +1,23 @@
+// https://edabit.com/challenge/3LpBLgNRyaHMvNb4j
+// Return the sum of Two Numbers
+
+function addition(a, b) {
+    return console.log(a + b)
+}
+addition(1,2)
+
+// https://edabit.com/challenge/8q54MKnRrm89pSLmW
+// Convert minutes into seconds
+
+function minToSec(minutes) {
+    return console.log(minutes * 60)
+}
+
+
+// https://edabit.com/challenge/NAQhEoxbofPidLxm9
+// Return the Next number from the Integer passed
+
+function addition2(num) {
+    return console.log(num + 1)
+}
+addition2(999)
