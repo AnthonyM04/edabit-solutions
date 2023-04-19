46acaa4d1075ea8f3393ebc914d77438edab7372
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-04-19 11:06:47 -0400

    very easy

---
+++ very-easy.js
@@ -0,0 +1,47 @@
+// https://edabit.com/challenge/3LpBLgNRyaHMvNb4j
+// Return the sum of Two Numbers
+
+function addition(a, b) {
+    return console.log(`The sum of the two numbers ${a} and ${b} is: ${a + b}!`)
+}
+addition(1,2)
+
+
+// https://edabit.com/challenge/8q54MKnRrm89pSLmW
+// Convert minutes into seconds
+
+function minToSec(minutes) {
+    const result = minutes * 60
+    return console.log(result)
+}
+minToSec(12)
+
+
+// https://edabit.com/challenge/NAQhEoxbofPidLxm9
+// Return the Next number from the Integer passed
+
+function nextNum(num) {
+    const result = (num + 1)
+    return console.log(result)
+}
+nextNum(999)
+
+
+// https://edabit.com/challenge/3CaszbdZYGN4otQD8
+// Area of A Triangle
+
+function triArea(base, height) {
+    const result = (base * height) / 2
+    return console.log(result)
+}
+triArea()
+
+// https://edabit.com/challenge/bL7hSc6Zh4zZJzGmw
+// Convert Age to Days
+
+function ageToDays(age) {
+    const result = age*365
+    return console.log(result)
+}
+
+ageToDays(18)
